80511be9ee84e326994724da13ff34d962611386
James <jd748@cornell.edu>
2015-05-09 19:09:36 -0400

    fuckboi

--- Random.cpp
+++ Random.cpp
@@ -16,8 +16,10 @@ int main()
     std::mt19937 generator2 (16746);
     std::normal_distribution <double> nd(0.0, 1.0);
 
-    double s [n][n];
+    double s [p][p];
     double zee [n][p];
+    double Z[n][p];
+    double mu [p];
 
     for (int i = 0; i < n; i++){
         for (int j = 0; j < p; j++){
@@ -31,6 +33,18 @@ int main()
         }
     }
 
+    double temp = 0;
 
-       
+    //Z is matrix of patient attributes (n x p)
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < p; j++){ 
+            temp = 0;
+
+            for (int k = 0; k < p; k++){ 
+                temp += s[k][j]*zee[i][k];
+            }
+                Z[i][j] = temp + mu[j];
+        } 
+    }
 }
+
