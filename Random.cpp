20c292997cc7a3c6d33bdff81d2c2a3ab85d66cb
jd748 <jd748@cornell.edu>
2015-05-26 14:26:14 -0400

    updates

--- Random.cpp
+++ Random.cpp
@@ -14,7 +14,10 @@ int main()
 
     std::mt19937 generator1 (61245);
     std::mt19937 generator2 (16746);
+    std::mt19937 generator3 (27351);
+    std::mt19937 generator4 (66459);
     std::normal_distribution <double> nd(0.0, 1.0);
+    std::chi_squared_distribution <double> xs(p - 2);
 
     double s [p][p];
     double zee [n][p];
@@ -45,6 +48,37 @@ int main()
             }
                 Z[i][j] = temp + mu[j];
         } 
+    }
+
+    //Eta + Xi computation
+    
+    double eta [n];
+    double xi [n];
+
+    for (int i = 0; i < n; i++){
+        eta[i] = nd(generator3);
+        xi[i] = xs(generator4);
+    }
+
+    //Solving 2-D DP using mesh
+    M = 2000.0;
+    delta = 0.2;
+    N = 10000;
+
+    double table [2*n+1][N][n];
+    
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < 2*n+1;j++){
+            table[j][i][0] = (-n + i)^2 + (0 + delta*j);
+        }
+    }
+    
+    for (int k = 1;k < n; k++){
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < 2*n + 1; j++){
+                                    
+            }
+        }
     }
 }
 
